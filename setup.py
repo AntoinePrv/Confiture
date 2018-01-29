5fffa3d635b8e98fc8cbd577e0c4357533cc66c0
AntoinePrv <antoine.prouvost.2013_dev@polytechnique.org>
2018-01-29 17:48:10 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='Confiture',
+    version='0.1',
+    packages=['confiture'],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
